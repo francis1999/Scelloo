056cbe021d1a90c6d5a89d88678a11151b3c363b
zercominternalrepo <repo@zercomsystems.com>
2022-07-16 19:52:51 +0100

    changing of db name

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ const dbConn = mysql.createConnection({
     host: "us-cdbr-east-06.cleardb.net",
     user: "b9b6da5f5ce1e8",
     password: "bee92be3",
-    database: "scelloo"
+    database: "heroku_8439ef7d3a0651e"
 });
 /* const dbConn = mysql.createConnection({
     host: "localhost",
