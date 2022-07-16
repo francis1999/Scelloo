04e7150d9266ecc57c29849ee4c02e5846b674d4
zercominternalrepo <repo@zercomsystems.com>
2022-07-16 19:28:50 +0100

    database configuratioin

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql');
 const dbConn = mysql.createConnection({
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b9b6da5f5ce1e8",
+    password: "bee92be3.",
+    database: "scelloo"
+});
+/* const dbConn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password12345.",
     database: "scellrr"
-});
+}); */
 
 
 dbConn.connect(function (error) {
